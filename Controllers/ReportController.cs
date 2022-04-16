94ed9112c12b54118a21501ea837bc4fae56cae6
Lynn Simonen <lynnsimonen@yahoo.com>
2022-04-16 00:22:14 -0500

    ReportOne js and chart.js

--- Controllers/ReportController.cs
+++ Controllers/ReportController.cs
@@ -14,18 +14,21 @@ namespace Northwind.Controllers
          // this controller depends on the NorthwindRepository
         private NorthwindContext _northwindContext;
         public ReportController(NorthwindContext db) => _northwindContext = db;
-
-
         
        //public IActionResult ReportOne() => View(_NorthwindContext.Categories.OrderBy(b => b.CategoryName));
   
        public IActionResult ReportOne() {
            List<Category> categories = _northwindContext.Categories.OrderBy(b => b.CategoryId).ToList();
-           //ViewBag.id;
            return View(categories);
        }
-        public IActionResult ReportTwo() => View(_northwindContext.Discounts.OrderBy(b => b.DiscountId));
-
-    
+        [HttpGet]
+        public Category GetCategory(int id)
+        {
+            Category chosenCategory = _northwindContext.Categories.Where(c => c.CategoryId == id).FirstOrDefault();
+            ViewBag.id = id;
+            return chosenCategory;
+        }
+        
+        public IActionResult ReportTwo() => View(_northwindContext.Discounts.OrderBy(b => b.DiscountId));    
     }
 }
